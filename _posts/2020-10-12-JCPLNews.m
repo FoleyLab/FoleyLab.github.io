7c662409ac217bbd49751f334f8f12b74b8c9084
FoleyLab <jayfoleyiv@gmail.com>
2021-06-09 11:49:33 -0400

    Create 2020-10-12-JCPLNews.m

---
+++ _posts/2020-10-12-JCPLNews.m
@@ -0,0 +1,23 @@
+---
+layout: post
+image: /images/ijqc_toc.jpg
+link: https://onlinelibrary.wiley.com/toc/1097461x/0/0
+title: "Our paper describing the role of cavity losses in polaritonic chemistry is out in JPC Letters!"
+authors: "Panayiota Antoniou, Figen Suchanek, James F. Varner, Jonathan J. FOley IV"
+journal: "J. Phys. Chem. Lett."
+year: 2020
+date: 2020-10-12
+category: post
+---
+We present a non-Hermitian formulation of the polaritonic structure 
+of azobenzene strongly coupled to a photonic mode that explicitly 
+accounts for the fleeting nature of the photon–molecule interaction. 
+  This formalism reveals that the polaritonic nonadiabatic couplings 
+  that facilitate cis–trans isomerization can be dramatically modified by 
+  photonic dissipation. We perform Fewest-Switches Surface Hopping 
+  dynamics on the surfaces that derive from our non-Hermitian formalism 
+  and find that the polaritonic isomerization yields are strongly suppressed 
+  for moderate dissipation rates and that cavity-free isomerization dynamics 
+  are recovered under large dissipation rates. These findings highlight the 
+  important role that the finite lifetime of photonic degrees of freedom play 
+  in polaritonic chemistry.
